e6e0e6ef3a4e44eb8382318cf98067c3a04785a8
Gustavo Dias <gustavoalvesdiaz08@gmail.com>
2024-07-23 16:24:15 -0300

    chore: to deploy in Vercel

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 // Components
 import StoreList from "../../components/StoreList";
 
--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,6 +1,3 @@
-// Styles
-import * as S from "./styles";
-
 // Components
 import MenuList from "../../components/MenuList";
  
